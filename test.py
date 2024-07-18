c9a157e71f7f16bd1801176ac7ee869941149550
freakfan15 <vivekkrrai.bxr@gmail.com>
2024-07-18 12:30:32 +0530

    rm unused

--- test.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
-
-# Load the original Excel file
-input_file_path = 'output.xlsx'
-output_file_path = './copied_output_with_formatting.xlsx'
-
-workbook = load_workbook(input_file_path)
-workbook.save(output_file_path)
